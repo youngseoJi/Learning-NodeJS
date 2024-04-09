394a088fd0e5bf16a419b51c519b68b291885153
지영서 <estell.ys.j@gmail.com>
2024-04-09 17:12:38 +0900

    json

---
+++ NodeServer/3-json.js
@@ -0,0 +1,46 @@
+/**
+ * json으로 데이터 주고 받기
+ */
+
+const http = require("http");
+
+const courses = [
+  { name: "HTML" },
+  { name: "CSS" },
+  { name: "JS" },
+  { name: "Node" },
+];
+
+// const http2 = require("http2"); //http2는 https를 사용할때 사용
+const server = http.createServer((req, res) => {
+  const url = req.url; // what? 무엇을 요청했는지
+  const method = req.method; // how?, action? 어떤 방식으로 요청했는지, 무엇을 받아 왔는지
+
+  if (url === "/courses") {
+    if (method === "GET") {
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      // courses의 객체 데이터를 json으로 변환해서 보내준다.
+      res.end(JSON.stringify(courses));
+    }
+    // POST 요청일때 : 새로운 데이터를 추가할때
+    else if (method === "POST") {
+      const body = [];
+      req.on("data", (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+      });
+
+      // 모든 데이터가 다 받아질때
+      req.on("end", () => {
+        const course = JSON.parse(Buffer.concat(body).toString());
+        courses.push(course);
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end();
+      });
+    }
+  }
+});
+
+server.listen(8000);
