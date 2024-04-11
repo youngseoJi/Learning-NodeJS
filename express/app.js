9b6a2361f5b5f6853a7a776b8d92ce2f945a8ebe
지영서 <estell.ys.j@gmail.com>
2024-04-11 15:39:24 +0900

    Add : express 사용하기 - 응답 데이터 보내는 방법 알아보기

--- express/app.js
+++ express/app.js
@@ -19,8 +19,15 @@ app.get("/we/:id", (req, res, next) => {
   console.log(req.query);
   console.log(req.query.keyword);
 
-  // res.send : 클라이언트에게 응답을 보낸다.
-  res.send("응답 완료");
+  // res : 응답에 대한 정보를 담고 있는 객체
+  // 응답 데이터를 json 형식, 상태값만, 상태값과 데이터를 같이 보내는 등 다양한 형식으로 보낼수 있다.
+
+  // res.send("데이터");
+  // res.json({ name: "ys", age: 20 });
+  // res.sendStatus(201);
+  // 필요시 헤더에 데이터를 추가하여 보낼 수 있음
+  res.setHeader("key", "value");
+  res.status(201).send("created");
 });
 
 // port 라우팅 설정 , 서버를 지정된 포트에서 실행한다.
