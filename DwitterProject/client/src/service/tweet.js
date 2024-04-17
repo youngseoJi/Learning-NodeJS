f3c56965a0df481a98b4c59d2ee937091563eb05
지영서 <estell.ys.j@gmail.com>
2024-04-17 11:09:00 +0900

    Update/Add : 클라 이언트 코드 수정 및 http클라이언트 클래스 추가
     http 클라이언트 클래스
     * 코드의 재사용성과 유지보수를 용이하게 하기 위한 클래스
     * 중앙 집중식 관리: 일관된 요청 처리 방식을 제공합니다.
     * 재사용성: fetch 메소드를 통해 다양한 서비스에서 재사용 가능합니다.
     * 에러 처리: 네트워크 요청 시 발생할 수 있는 에러를 일관된 방식으로  처리합니다.
     * 유연성: 요청에 필요한 기본 옵션을 설정하고, 필요에 따라 변경할 수  있습니다.

--- DwitterProject/client/src/service/tweet.js
+++ DwitterProject/client/src/service/tweet.js
@@ -1,80 +1,41 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http) {
+    this.http = http;
   }
-  // 1. 전체 트윗 갖고오기 Get /tweets
-  // 2. 특정 유저 트윗 갖고 오기 Get /tweets?username=:username
+  // 1. 전체 트윗 갖고오기
+  // 2. 특정 유저 트윗 갖고 오기
 
   async getTweets(username) {
     const query = username ? `?username=${username}` : "";
-    const response = await fetch(`${this.baseURL}/tweets${query}`, {
+    return this.http.fetch(`/tweets${query}`, {
       method: "GET",
-      headers: { "Content-Type": "application/json" },
     });
-
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
-  // // 3. 특정 트윗 조회 Get /tweets/:id
-  async getTweet(tweetId) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
-
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
-  }
-
-  // 4. 새로운 트윗 생성 POST /tweets
+  // 3. 새로운 트윗 생성
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/tweets`, {
+    return this.http.fetch(`/tweets`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         text,
         username: "estell",
         name: "estell",
       }),
     });
-    const data = await response.json();
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-    console.log(data);
-    return data;
   }
 
-  // 5. 트윗 삭제 DELETE /tweets/:id
+  // 4. 트윗 삭제
   async deleteTweet(tweetId) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: "DELETE",
-      headers: { "Content-Type": "application/json" },
     });
-
-    if (response.status !== 204) {
-      throw new Error("Tweet not found");
-    }
   }
-  // 6. 트윗 수정 PUT /tweets/:id
+
+  // 5. 트윗 수정 PUT
   async updateTweet(tweetId, text) {
-    const response = await fetch(`${this.baseURL}/tweets${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: "PUT",
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
     });
-
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 }
